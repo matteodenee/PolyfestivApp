de02e44e25dda923481380268ce55425a4e4f2f8
Myndie <myndie.ferrandez@etu.umontpellier.fr>
2026-03-14 17:09:35 +0100

    feat: add function register to AuthRepository

--- app/src/main/java/com/example/clicker/data/auth/AuthRepository.kt
+++ app/src/main/java/com/example/clicker/data/auth/AuthRepository.kt
@@ -2,9 +2,18 @@ package com.example.clicker.data.auth
 
 class AuthRepository(private val api: AuthApiService) {
 
-    suspend fun login(login: String, password: String): LoginResponse {
+    suspend fun login(login: String, password: String): AuthResponse {
         return api.login(
             LoginRequest(
+                login = login,
+                password = password
+            )
+        )
+    }
+
+    suspend fun register(login: String, password: String): AuthResponse {
+        return api.register(
+            RegisterRequest(
                 login = login,
                 password = password
             )
