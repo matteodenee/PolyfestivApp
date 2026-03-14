609fdcef67b609c8bb3a18045ce2cd72efdad549
Myndie <myndie.ferrandez@etu.umontpellier.fr>
2026-03-14 17:10:42 +0100

    fix: change name LoginResponse in AuthResponse

--- app/src/main/java/com/example/clicker/data/auth/AuthResponse.kt
+++ app/src/main/java/com/example/clicker/data/auth/AuthResponse.kt
@@ -3,7 +3,7 @@ package com.example.clicker.data.auth
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class LoginResponse(
+data class AuthResponse(
     val message: String,
     val user: UserDto
 )
