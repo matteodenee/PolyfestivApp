fddb2903e71309bf44315791e9a2846ac831e334
Myndie <myndie.ferrandez@etu.umontpellier.fr>
2026-03-14 15:42:50 +0100

    fix: update ClickerNavHost

--- app/src/main/java/com/example/clicker/ui/navigation/ClickerNavHost.kt
+++ app/src/main/java/com/example/clicker/ui/navigation/ClickerNavHost.kt
@@ -1,14 +1,13 @@
 package com.example.clicker.ui.navigation
 
+
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import com.example.clicker.ui.AppViewModelProvider
+import com.example.clicker.ui.screens.MainScreen
 import com.example.clicker.ui.screens.login.LoginScreen
-import com.example.clicker.ui.screens.login.LoginViewModel
 
 @Composable
 fun ClickerNavHost(
@@ -21,7 +20,17 @@ fun ClickerNavHost(
         modifier = modifier
     ) {
         composable(route = LoginDestination.route) {
-            LoginScreen()
+            LoginScreen(
+                onLoginSuccess = {
+                    navController.navigate("main") {
+                        popUpTo(LoginDestination.route) { inclusive = true }
+                    }
+                }
+            )
+        }
+
+        composable(route = "main") {
+            MainScreen()
         }
     }
 }
