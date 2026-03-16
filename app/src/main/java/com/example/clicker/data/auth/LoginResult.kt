ccada067e061cfc9f4d05dceaee7bfd5740de35f
Myndie <myndie.ferrandez@etu.umontpellier.fr>
2026-03-16 16:50:04 +0100

    feat: add accescookie and refreshCookie to LoginResult

--- app/src/main/java/com/example/clicker/data/auth/LoginResult.kt
+++ app/src/main/java/com/example/clicker/data/auth/LoginResult.kt
@@ -2,5 +2,6 @@ package com.example.clicker.data.auth
 
 data class LoginResult(
     val response: AuthResponse,
-    val cookie: String?
+    val accessCookie: String?,
+    val refreshCookie: String?
 )
