3dc965fe5a09a1e79f4dfa760013090cccf71f9e
Myndie <myndie.ferrandez@etu.umontpellier.fr>
2026-03-18 19:02:41 +0100

    feat: add Room database support to GamesRepository

--- app/src/main/java/com/example/clicker/data/AppDataContainer.kt
+++ app/src/main/java/com/example/clicker/data/AppDataContainer.kt
@@ -1,17 +1,23 @@
 package com.example.clicker.data
 
+import android.content.Context
 import com.example.clicker.data.auth.AuthRepository
 import com.example.clicker.data.game.GamesRepository
+import com.example.clicker.data.local.game.GameDatabase
 import com.example.clicker.data.network.RetrofitInstance
 
-
-class AppDataContainer : AppContainer {
+class AppDataContainer(
+    private val context: Context
+) : AppContainer {
 
     override val authRepository: AuthRepository by lazy {
         AuthRepository(RetrofitInstance.authApi)
     }
 
     override val gamesRepository: GamesRepository by lazy {
-        GamesRepository(RetrofitInstance.gamesApi)
+        GamesRepository(
+            api = RetrofitInstance.gamesApi,
+            gameDao = GameDatabase.getDatabase(context).gameDao()
+        )
     }
 }
