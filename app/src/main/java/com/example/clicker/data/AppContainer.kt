362a602135bbefd5273ea3981b799e48ab99623c
Myndie <myndie.ferrandez@etu.umontpellier.fr>
2026-03-23 09:35:13 +0100

    feat: add adminRepository in AppContainer

--- app/src/main/java/com/example/clicker/data/AppContainer.kt
+++ app/src/main/java/com/example/clicker/data/AppContainer.kt
@@ -3,8 +3,10 @@ package com.example.clicker.data
 
 import com.example.clicker.data.auth.AuthRepository
 import com.example.clicker.data.game.GamesRepository
+import com.example.clicker.data.admin.AdminRepository
 
 interface AppContainer {
     val authRepository: AuthRepository
     val gamesRepository: GamesRepository
+    val adminRepository: AdminRepository
 }
