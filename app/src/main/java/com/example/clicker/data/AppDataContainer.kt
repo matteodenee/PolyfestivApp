7c1fa1f5736e8d0d8bd2ceea00446bbe88f9f188
Myndie <myndie.ferrandez@etu.umontpellier.fr>
2026-03-23 09:35:28 +0100

    feat: add adminRepository in AppDataContainer

--- app/src/main/java/com/example/clicker/data/AppDataContainer.kt
+++ app/src/main/java/com/example/clicker/data/AppDataContainer.kt
@@ -5,6 +5,7 @@ import com.example.clicker.data.auth.AuthRepository
 import com.example.clicker.data.game.GamesRepository
 import com.example.clicker.data.local.game.GameDatabase
 import com.example.clicker.data.network.RetrofitInstance
+import com.example.clicker.data.admin.AdminRepository
 
 class AppDataContainer(
     private val context: Context
@@ -19,5 +20,9 @@ class AppDataContainer(
             api = RetrofitInstance.gamesApi,
             gameDao = GameDatabase.getDatabase(context).gameDao()
         )
+    }
+
+    override val adminRepository: AdminRepository by lazy {
+        AdminRepository(RetrofitInstance.adminApi)
     }
 }
