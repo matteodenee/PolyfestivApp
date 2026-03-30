fe37ee28691048a5556011045812ec5ce860ea56
Myndie <myndie.ferrandez@etu.umontpellier.fr>
2026-03-30 22:03:46 +0200

    feat: add reservationsRepository into AppContainer

--- app/src/main/java/com/example/clicker/data/AppContainer.kt
+++ app/src/main/java/com/example/clicker/data/AppContainer.kt
@@ -4,10 +4,12 @@ import com.example.clicker.data.auth.AuthRepository
 import com.example.clicker.data.exposants.ExposantRepository
 import com.example.clicker.data.game.GamesRepository
 import com.example.clicker.data.admin.AdminRepository
+import com.example.clicker.data.reservation.ReservationsRepository
 
 interface AppContainer {
     val authRepository: AuthRepository
     val gamesRepository: GamesRepository
     val adminRepository: AdminRepository
     val exposantRepository: ExposantRepository
+    val reservationsRepository: ReservationsRepository
 }
