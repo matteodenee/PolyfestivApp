f6c678eb9e930f662c26f3aecbed9ed9cffb6e6b
Myndie <myndie.ferrandez@etu.umontpellier.fr>
2026-03-30 22:01:42 +0200

    feat: add initializer ReservationsViewModel

--- app/src/main/java/com/example/clicker/ui/viewmodel/AppViewModelProvider.kt
+++ app/src/main/java/com/example/clicker/ui/viewmodel/AppViewModelProvider.kt
@@ -13,6 +13,7 @@ import com.example.clicker.ui.screens.games.GamesViewModel
 import com.example.clicker.ui.screens.login.LoginViewModel
 import com.example.clicker.ui.screens.register.RegisterViewModel
 import com.example.clicker.ui.screens.admin.AdminViewModel
+import com.example.clicker.ui.screens.reservations.ReservationsViewModel
 
 object AppViewModelProvider {
     val Factory = viewModelFactory {
@@ -62,13 +63,21 @@ object AppViewModelProvider {
                 clickerApplication().container.adminRepository
             )
         }
-        
+
         initializer {
             ExposantViewModel(
                 repository = clickerApplication().container.exposantRepository
             )
         }
-          
+
+        initializer {
+            ReservationsViewModel(
+                clickerApplication().container.reservationsRepository,
+                clickerApplication().container.festivalsRepository,
+                clickerApplication().container.exposantRepository
+            )
+        }
+
     }
 }
 
