18f690b64d6595fa40fe230875b0407f8364b69f
Myndie <myndie.ferrandez@etu.umontpellier.fr>
2026-03-30 22:37:57 +0200

    feat: add ReservationCreateUiState

---
+++ app/src/main/java/com/example/clicker/ui/screens/reservationCreate/ReservationCreateUiState.kt
@@ -0,0 +1,8 @@
+package com.example.clicker.ui.screens.reservationCreate
+
+sealed interface ReservationCreateUiState {
+    data object Idle : ReservationCreateUiState
+    data object Loading : ReservationCreateUiState
+    data object Success : ReservationCreateUiState
+    data class Error(val message: String) : ReservationCreateUiState
+}
