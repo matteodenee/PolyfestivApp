0dde4508cbe7b54a3f392e949715e7d7a910d93b
Myndie <myndie.ferrandez@etu.umontpellier.fr>
2026-03-31 08:36:29 +0200

    feat: add ReservationDetail et reservationSupplies routes

--- app/src/main/java/com/example/clicker/ui/navigation/AppRoutes.kt
+++ app/src/main/java/com/example/clicker/ui/navigation/AppRoutes.kt
@@ -1,5 +1,7 @@
 package com.example.clicker.ui.navigation
 
+import com.example.clicker.data.reservation.ReservationDto
+
 sealed interface AppRoutes {
     data object LOGIN : AppRoutes
     data object REGISTER : AppRoutes
@@ -13,4 +15,6 @@ sealed interface AppRoutes {
     data class ExposantEditRoute(val exposantId: Int) : AppRoutes
     data class ReservationsRoute(val festivalId: Int) : AppRoutes
     data class ReservationCreateRoute(val festivalId: Int) : AppRoutes
+    data class ReservationDetailRoute(val reservation: ReservationDto) : AppRoutes
+    data class ReservationSuppliesRoute(val reservation: ReservationDto) : AppRoutes
 }
