ef56474764827ce2904ec1b472c0614a2a5460ba
Myndie <myndie.ferrandez@etu.umontpellier.fr>
2026-04-05 23:43:24 +0200

    feat: update AppContainer

--- app/src/main/java/com/example/clicker/data/AppContainer.kt
+++ app/src/main/java/com/example/clicker/data/AppContainer.kt
@@ -4,10 +4,20 @@ import com.example.clicker.data.auth.AuthRepository
 import com.example.clicker.data.exposants.ExposantRepository
 import com.example.clicker.data.game.GamesRepository
 import com.example.clicker.data.admin.AdminRepository
+import com.example.clicker.data.equipment.EquipmentRepository
+import com.example.clicker.data.mapZone.MapZoneRepository
+import com.example.clicker.data.festivalTable.FestivalTableRepository
+import com.example.clicker.data.festivalEquipmentStock.FestivalEquipmentStockRepository
+import com.example.clicker.data.reservationGamePlacement.ReservationGamePlacementRepository
 
 interface AppContainer {
     val authRepository: AuthRepository
     val gamesRepository: GamesRepository
     val adminRepository: AdminRepository
     val exposantRepository: ExposantRepository
+    val equipmentRepository: EquipmentRepository
+    val mapZoneRepository: MapZoneRepository
+    val festivalTableRepository: FestivalTableRepository
+    val festivalEquipmentStockRepository: FestivalEquipmentStockRepository
+    val reservationGamePlacementRepository: ReservationGamePlacementRepository
 }
