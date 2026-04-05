7310b5e41115d635d799b8c98fd4a026dcde3186
Myndie <myndie.ferrandez@etu.umontpellier.fr>
2026-04-05 23:44:24 +0200

    feat: update AppDataContainer

--- app/src/main/java/com/example/clicker/data/AppDataContainer.kt
+++ app/src/main/java/com/example/clicker/data/AppDataContainer.kt
@@ -7,6 +7,11 @@ import com.example.clicker.data.game.GamesRepository
 import com.example.clicker.data.local.game.GameDatabase
 import com.example.clicker.data.network.RetrofitInstance
 import com.example.clicker.data.admin.AdminRepository
+import com.example.clicker.data.equipment.EquipmentRepository
+import com.example.clicker.data.mapZone.MapZoneRepository
+import com.example.clicker.data.festivalTable.FestivalTableRepository
+import com.example.clicker.data.festivalEquipmentStock.FestivalEquipmentStockRepository
+import com.example.clicker.data.reservationGamePlacement.ReservationGamePlacementRepository
 
 class AppDataContainer(
     private val context: Context
@@ -29,5 +34,24 @@ class AppDataContainer(
 
     override val adminRepository: AdminRepository by lazy {
         AdminRepository(RetrofitInstance.adminApi)
+    }
+
+    override val equipmentRepository: EquipmentRepository by lazy {
+        EquipmentRepository(RetrofitInstance.equipmentApi)
+    }
+    override val mapZoneRepository: MapZoneRepository by lazy {
+        MapZoneRepository(RetrofitInstance.mapZoneApi)
+    }
+
+    override val festivalTableRepository: FestivalTableRepository by lazy {
+        FestivalTableRepository(RetrofitInstance.festivalTableApi)
+    }
+
+    override val festivalEquipmentStockRepository: FestivalEquipmentStockRepository by lazy {
+        FestivalEquipmentStockRepository(RetrofitInstance.festivalEquipmentStockApi)
+    }
+
+    override val reservationGamePlacementRepository: ReservationGamePlacementRepository by lazy {
+        ReservationGamePlacementRepository(RetrofitInstance.reservationGamePlacementApi)
     }
 }
