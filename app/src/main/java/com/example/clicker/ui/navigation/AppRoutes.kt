aef15f4c7d05eea4d91ef4fabcc826669e90c5ed
Myndie <myndie.ferrandez@etu.umontpellier.fr>
2026-04-06 09:13:52 +0200

    feat: add publicPlanRoute

--- app/src/main/java/com/example/clicker/ui/navigation/AppRoutes.kt
+++ app/src/main/java/com/example/clicker/ui/navigation/AppRoutes.kt
@@ -11,4 +11,5 @@ sealed interface AppRoutes {
     data object ExposantCreateRoute : AppRoutes
     data class ExposantDetailRoute(val exposantId: Int) : AppRoutes
     data class ExposantEditRoute(val exposantId: Int) : AppRoutes
+    data class PublicPlanRoute(val festivalId: Int) : AppRoutes
 }
